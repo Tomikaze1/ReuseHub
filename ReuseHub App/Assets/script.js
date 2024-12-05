8565cecc4f3c12850c9e65c4c44ec294b1a7debf
Tomikaze1 <Jeanclaude36@gmail.com>
2024-12-05 14:05:30 +0800

    added new changes and bug fixes YAWAA

---
+++ ReuseHub App/Assets/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('click', function(event) {
+    const menu = document.getElementById('dropdownMenu');
+    const button = document.querySelector('.menu-button');
+    if (!menu.contains(event.target) && event.target !== button) {
+        menu.style.display = 'none';
+    }
+  });
+  
+  function toggleMenu() {
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    dropdownMenu.style.display = dropdownMenu.style.display === 'flex' ? 'none' : 'flex';
+  }
+  
+  function logOut() {
+    window.location.href = 'login.html';
+  }
+  
